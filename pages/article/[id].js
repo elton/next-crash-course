08c07b9a81719d5ce1b36784e0fdaecd40ebdfa4
Elton Zheng <elton.zheng.sh@gmail.com>
2021-03-09 17:23:03 +0800

    Feat:add Incremental Static Regeneration feature.

--- pages/article/[id].js
+++ pages/article/[id].js
@@ -2,13 +2,21 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 const article = ({ article }) => {
-  return (
-    <div className='max-w-3xl'>
-      <h1 className='text-3xl font-bold mb-5'>{article.title}</h1>
-      <p className='text-gray-700 text-base'>{article.body}</p>
-      <Link href='/'>Go Back</Link>
-    </div>
-  );
+  const router = useRouter();
+
+  // If the page is not yet generated, this will be displayed
+  // initially until getStaticProps() finishes running
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div className='max-w-3xl'>
+        <h1 className='text-3xl font-bold mb-5'>{article.title}</h1>
+        <p className='text-gray-700 text-base'>{article.body}</p>
+        <Link href='/'>Go Back</Link>
+      </div>
+    );
+  }
 };
 
 // This function gets called at build time
